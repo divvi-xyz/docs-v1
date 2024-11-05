75f5a0b1d56d54aad84c5883b079ebf7aa69f93d
Silas Boyd-Wickizer <silasbw@gmail.com>
2024-11-05 09:17:13 -0800

    chore: format fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,54 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
 
 const config: Config = {
-  title: "Mobile Stack Docs",
-  tagline: "Documentation for building with Mobile Stack",
-  favicon: "img/favicon.ico",
+  title: 'Mobile Stack Docs',
+  tagline: 'Documentation for building with Mobile Stack',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://docs.mobilestack.xyz",
+  url: 'https://docs.mobilestack.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "mobilestack-xyz", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  organizationName: 'mobilestack-xyz', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
+          sidebarPath: './sidebars.ts',
           editUrl: ({ versionDocsDirPath, docPath }) => {
-            if (docPath.startsWith("hooks/")) {
+            if (docPath.startsWith('hooks/')) {
               return `https://github.com/valora-inc/hooks/edit/main/${versionDocsDirPath}/${docPath.replace(
                 /^hooks\//,
-                "",
-              )}`;
+                '',
+              )}`
             }
-            return `https://github.com/mobilestack-xyz/docs/edit/main/${versionDocsDirPath}/${docPath}`;
+            return `https://github.com/mobilestack-xyz/docs/edit/main/${versionDocsDirPath}/${docPath}`
           },
           // The default is /docs, but we don't have anything other content but docs.
-          routeBasePath: "/",
+          routeBasePath: '/',
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
@@ -57,7 +57,7 @@ const config: Config = {
   plugins: [
     function (context, options) {
       return {
-        name: "webpack-configuration-plugin",
+        name: 'webpack-configuration-plugin',
         configureWebpack(config, isServer, utils) {
           return {
             resolve: {
@@ -67,49 +67,49 @@ const config: Config = {
               // See https://github.com/facebook/docusaurus/issues/3272
               symlinks: false,
             },
-          };
+          }
         },
-      };
+      }
     },
   ],
 
   themeConfig: {
     navbar: {
-      title: "Mobile Stack Docs",
+      title: 'Mobile Stack Docs',
       logo: {
-        alt: "Mobile Stack Logo",
-        src: "img/mobile-stack-icon.svg",
+        alt: 'Mobile Stack Logo',
+        src: 'img/mobile-stack-icon.svg',
       },
       items: [
         {
-          href: "https://github.com/mobilestack-xyz/docs",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/mobilestack-xyz/docs',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "Discord",
-              href: "https://valoraapp.co/discord",
+              label: 'Discord',
+              href: 'https://valoraapp.co/discord',
             },
             {
-              label: "Twitter",
-              href: "https://www.twitter.com/valoraApp",
+              label: 'Twitter',
+              href: 'https://www.twitter.com/valoraApp',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/mobilestack-xyz",
+              label: 'GitHub',
+              href: 'https://github.com/mobilestack-xyz',
             },
           ],
         },
@@ -121,6 +121,6 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-};
+}
 
-export default config;
+export default config
